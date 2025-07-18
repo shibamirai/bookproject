1703b0f716afe2cdc7af34b4394c06e53ed7ea1f
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-07-18 15:42:17 +0900

    username と password に値をセット

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='').exists():
             User.objects.create_superuser(
-                username='',
+                username='user',
                 email='',
-                password=''
+                password='pass'
             )
